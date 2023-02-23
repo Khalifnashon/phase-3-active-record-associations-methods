e613f103d49b109d370096b88cb1f8b05eabc361
+ <+>
2023-02-23 20:11:46 +0300

    completed lab

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,18 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
+    artists.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    # We can use the pluck method to extract the name attribute from each artist 
+    # object and return an array of strings.
+    artists.pluck(:name)
   end
 end
